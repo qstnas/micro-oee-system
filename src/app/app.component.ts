b53dde4453cafd383a5b077d4adff9a21baee955
Qistina <qsulaihan@gmail.com>
2022-02-14 17:51:39 +0800

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,23 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+  // public appPages = [
+  //   { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
+  //   { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
+  //   { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
+  //   { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
+  //   { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
+  //   { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+  // ];
+  // public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  // constructor() {}
+
+  public pages: any[] = [
+    { title: 'Home', url: '/home', icon: 'home-outline' },
+    { title: 'Planner Sheet', url: '/planner-sheet', icon: 'folder-outline' },
+    { title: 'Charts', url: '/charts', icon: 'pie-chart-outline' },
+    { title: 'Settings', url: '/settings', icon: 'settings-outline' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+
   constructor() {}
 }
