0c53e812b4ee8c9a0ad89bd0abf46d645001932e
Sabrina Azwa <azw.sab@gmail.com>
2022-02-21 11:26:33 +0800

    deleted

--- src/app/home/operator-dashboard/operator-dashboard.page.spec.ts
+++ src/app/home/operator-dashboard/operator-dashboard.page.spec.ts
@@ -1,37 +1,22 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-import { AppRoutingModule } from 'src/app/app-routing.module';
 
 import { OperatorDashboardPage } from './operator-dashboard.page';
 
 describe('OperatorDashboardPage', () => {
   let component: OperatorDashboardPage;
   let fixture: ComponentFixture<OperatorDashboardPage>;
-  let router: Router;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ OperatorDashboardPage ],
       imports: [
-        IonicModule.forRoot(),
-        AppRoutingModule
+        IonicModule.forRoot()
       ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(OperatorDashboardPage);
-    router = TestBed.get(Router);
-
     component = fixture.componentInstance;
   }));
-
-  it('ahould go to operator sheet page on arrow1', () => {
-    spyOn(router, 'navigate');
-
-    component.arrow1();
-
-    expect(router.navigate).toHaveBeenCalledWith(['operator-sheet']);
-  });
-
 
 });
